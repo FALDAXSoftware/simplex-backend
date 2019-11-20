b5952988497c63ef2c72442bc3875afbe1dfb26a
Jagdish <jagdish.banda@openxcelltechnolabs.com>
2019-11-20 16:20:24 +0530

    Basic configuration

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,17 @@
 var express = require('express');
-// var router = express.Router();
+var router = express.Router();
 
 var app=express();
 
 
 // route grouping
-app.prefix('/api/v1/', function (router) { 
-    var v1 = require("./v1/common.route");
-    router.use('/',v1);
+// var SimplexController = require('../../controllers/v1/SimplexController');
+var UsersController = require('../controllers/v1/UsersController');
+
+router.get('/api/v1/users/get/:id', UsersController.get);
+router.get('/api/v1/test', function(req, res){
+    return res.json({status:1})
 });
 
-// CMS Routes
-app.prefix('/admin/', function (router) { 
-    var cms = require("./cms/cms.route");
-    router.use('/',cms);
-});
 
-app.use(function (req, res, next) {
-    var err = new Error('Resource Not Found');
-    err.status = 404;
-    var resources = {};
-    res.status( 404 );
-    resources.status = err.status;
-    resources.message = err.message;
-    return res.json(resources);
-});
-
-module.exports = app;
+module.exports = router;
