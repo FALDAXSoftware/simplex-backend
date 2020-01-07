e1ddeaeb07eb106a8376b548503faea400accf82
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-01-07 14:09:16 +0530

    simplex changes

--- services/cronJobs.js
+++ services/cronJobs.js
@@ -1,10 +1,3 @@
 /* Used to store CronJobs  */
 var cron = require('node-cron');
 var simplexController = require('../controllers/v1/SimplexController');
-
-// On Every Minute
-cron.schedule('* * * * *',async (req, res, next) => {
-    console.log("Started cron....");
-    await simplexController.checkPaymentStatus();
-    
-});
